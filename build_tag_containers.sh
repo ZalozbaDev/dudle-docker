05ba200634de16758be7505c3cad050087bbfc4a
Daniel Zoba <daniel.sobe@sorben.com>
2024-05-14 09:05:01 +0200

    remember what to check before container build

--- build_tag_containers.sh
+++ build_tag_containers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+#!!!!!!!!!!!!! check embedded GIT in cgi/ subfolder first (dudle repo!)
+
 docker build -t dudle-app .
 
 pushd scripts/maintenance/
@@ -9,7 +11,7 @@ docker build -t dudle-maint .
 
 popd
 
-# docker image tag dudle-app:latest registry/dudle-app:0.0.2
+# docker image tag dudle-app:latest registry/dudle-app:0.0.3
 # docker image tag dudle-maint:latest registry/dudle-maint:0.0.2
 
 
